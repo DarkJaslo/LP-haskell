3974f7f39c4911a622ceaab89a6c640f2d6fd8d7
Jon <jon.campillo@gmail.com>
2024-02-20 20:52:53 +0100

    did remove

--- P25054.hs
+++ P25054.hs
@@ -24,7 +24,21 @@ buildPalindrome' (x:xs) = (buildPalindrome' xs) ++ [x]
 buildPalindrome :: [Int] -> [Int]
 buildPalindrome (x:xs) = (buildPalindrome' (x:xs)) ++ (x:xs)
 
---remove :: [Int] -> [Int] -> [Int]
+--auxiliary for remove
+isIn :: [Int] -> Int -> Bool
+isIn [] x = False
+isIn [x] y = x==y
+isIn (x:xs) y = (isIn [x] y) || (isIn (xs) y)
+
+remove :: [Int] -> [Int] -> [Int]
+remove [] (y:ys) = []
+remove (x:xs) [] = (x:xs)
+remove (x:xs) (y:ys) = list
+    where
+        list 
+            |  isIn (y:ys) x = remove xs (y:ys)
+            |  otherwise = [x] ++ (remove xs (y:ys))
+
 
 --flatten :: [[Int]] -> [Int]
 
