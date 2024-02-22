c29bd8e5d6538af725b4fed86016b31e662c076a
Jon <jon.campillo@gmail.com>
2024-02-22 12:45:04 +0100

    accumulation alternative for average

--- P25054.hs
+++ P25054.hs
@@ -17,6 +17,21 @@ average (x:xs) = s / l
         s = fromIntegral (mySum (x:xs))
         l = fromIntegral (myLength (x:xs))
 
+{- Accumulation version
+average :: [Int] -> Float
+average l = uncurry g $ average' l
+    where
+        g :: Int -> Int -> Float
+        g x y = (fromIntegral x:: Float) / 
+                (fromIntegral y:: Float)
+        average' :: [Int] -> (Int,Int)
+        average' [x] = (x,1)
+        average (x:xs) = f x $ average' xs
+            where
+                f:: Int -> (Int,Int) -> (Int,Int)
+                f x t (x + fst t, 1 + snd t)
+-}
+
 buildPalindrome' :: [Int] -> [Int]
 buildPalindrome' [x] = [x]
 buildPalindrome' (x:xs) = (buildPalindrome' xs) ++ [x]
